90debdca31fc7f7e140c866c0275d6e3b2cd9e4e
FonDitbul <niki7084@naver.com>
2022-02-28 00:24:40 +0000

    typescript 설정

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+class App{
+	app: express.Application;
+	constructor() {
+		this.app = express();
+	}
+}
+
+const app = new App().app;
+app.get('/', (req:express.Request, res:express.Response)=>{
+	res.send('Hello');
+})
+app.listen(8080, () => {
+	console.log('listen express server!');
+})
